ea8b08d9e1ef1475116a42f3e57143802c12c04c
mtmnikki <nikki@mtmtft.com>
2025-08-20 11:05:30 -0500

    Delete utils directory

--- utils/cn.ts
+++
@@ -1,7 +0,0 @@
-/**
- * cn utility
- * - Minimal className joiner with conditional support.
- */
-export function cn(...args: Array<string | false | null | undefined>) {
-  return args.filter(Boolean).join(' ');
-}
